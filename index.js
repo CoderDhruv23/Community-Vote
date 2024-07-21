3d71bafb5d03341d0ebee10d5affad51ef739cdb
Vasundhara Dixit <vdixit2505@gmail.com>
2024-07-21 11:27:54 +0530

    basic nav done

--- index.js
+++ index.js
@@ -3,35 +3,39 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-
-// Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'server', 'routes'));
+app.set('views', [path.join(__dirname, 'views'), path.join(__dirname, 'server/routes')]);
 
-// Serve static files
+// Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define a route
+// Route for rendering the EJS template
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.render('main', {
+    title: 'Community Vote'
+  });
 });
+
+
+
+
+
 
 // Error handling for 404 - Not Found
 app.use((req, res, next) => {
-    res.status(404).send('Sorry, we cannot find that!');
+  res.status(404).send('Sorry, we cannot find that!');
 });
 
 // Error handling for other errors
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
